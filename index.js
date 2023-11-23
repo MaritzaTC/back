f0514cf36514bd226a390f1b5a7588b1f9ead824
MaritzaTC <maritza.tabarezc@udea.edu.co>
2023-11-23 00:39:43 -0500

    Add cors middleware and update dependencies

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import driversRoutes from './src/routes/drivers.routes.js';
 import {PORT} from'./src/config.js'
+import cors from 'cors';
 const app = express();
 
 
+app.use(cors());
 app.use(express.json());
 app.use("/api",driversRoutes);
 
